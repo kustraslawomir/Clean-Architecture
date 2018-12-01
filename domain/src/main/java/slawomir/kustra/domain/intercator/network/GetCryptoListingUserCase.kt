d0b91efc81fe42a7d6812c420dd5e6a49cbe3e8c
skustra <kustraslawomir@gmail.com>
2018-12-01 11:20:25 +0100

    crypto listing use case

---
+++ domain/src/main/java/slawomir/kustra/domain/intercator/network/GetCryptoListingUserCase.kt
@@ -0,0 +1,16 @@
+package slawomir.kustra.domain.intercator.network
+
+import io.reactivex.Observable
+import slawomir.kustra.domain.executor.PostExecutionThread
+import slawomir.kustra.domain.intercator.ObservableUseCase
+import slawomir.kustra.domain.model.listing.CryptoListingResponse
+import slawomir.kustra.domain.repository.Repository
+
+class GetCryptoListingUserCase constructor(private val repository: Repository,
+                                           private val postExecutionThread: PostExecutionThread)
+    : ObservableUseCase<CryptoListingResponse, Nothing?>(postExecutionThread) {
+
+
+    override fun getObservableUseCase(params: Nothing?): Observable<CryptoListingResponse> =
+            repository.getCryptoCurrencyListing()
+}
--- domain/src/main/java/slawomir/kustra/domain/repository/Repository.kt
+++ domain/src/main/java/slawomir/kustra/domain/repository/Repository.kt
@@ -1,16 +1,16 @@
 package slawomir.kustra.domain.repository
 
-import io.reactivex.Single
+import io.reactivex.Observable
 import slawomir.kustra.domain.model.listing.Coin
 import slawomir.kustra.domain.model.listing.CryptoListingResponse
 
 interface Repository {
 
-    fun getCryptoCurrencyListing(): Single<CryptoListingResponse>
+    fun getCryptoCurrencyListing(): Observable<CryptoListingResponse>
 
-    fun pinCoin(id: Int) : io.reactivex.Completable
+    fun observeCurrency(id: Int) : io.reactivex.Completable
 
-    fun unPinCoin(id: Int) : io.reactivex.Completable
+    fun stopObservingCurrency(id: Int) : io.reactivex.Completable
 
-    fun getPinnedCoins() : io.reactivex.Observable<Coin>
+    fun getObservedCurrencies() : io.reactivex.Observable<Coin>
 }
