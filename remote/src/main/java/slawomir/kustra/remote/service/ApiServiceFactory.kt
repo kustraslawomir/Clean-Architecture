a35494689292871cf8cf499a42fe818a1f7c8b9b
skustra <kustraslawomir@gmail.com>
2018-12-06 18:11:57 +0100

    api service factory

---
+++ remote/src/main/java/slawomir/kustra/remote/service/ApiServiceFactory.kt
@@ -0,0 +1,33 @@
+package slawomir.kustra.remote.service
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+class ApiServiceFactory {
+
+    private fun makeApiService(): ApiService {
+        val okHttpClient = getOkHttpClient(getHttpLoggingInterceptor())
+        return getRetrofitService(okHttpClient)
+    }
+
+    private fun getRetrofitService(okHttpClient: OkHttpClient): ApiService {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://pro-api.coinmarketcap.com/v1/cryptocurrency/")
+                .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+
+    private fun getOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor) =
+            OkHttpClient.Builder()
+                    .addInterceptor(httpLoggingInterceptor)
+                    .build()
+
+    private fun getHttpLoggingInterceptor() =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+}
